21fcc71e2520bbc6ab1a057702331dc6d7b3c352
tein408 <tein408@gmail.com>
2021-09-10 21:00:58 +0900

    add BOJ 9020

---
+++ src/P9020.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class P9020 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(bf.readLine());
+
+        boolean[] prime = new boolean[10000];
+        prime[0] = true;
+        prime[1] = true;
+
+        for(int i=2; i<10000; i++) {
+            if(!prime[i]) {
+                for(int j=i+i; j<10000; j+=i) prime[j] = true;
+            }
+        }
+
+        for(int k=0; k<num; k++) {
+            int n = Integer.parseInt(bf.readLine());
+            for(int j=n/2;;j--) {
+                if (!prime[j] && !prime[n-j]) {
+                    System.out.println(j + " " + (n - j));
+                    break;
+                }
+            }
+        }
+    }
+
+}
