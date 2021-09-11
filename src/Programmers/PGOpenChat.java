a269fb1aa97c22dde16702f83ac2b195bdc3a141
tein408 <tein408@gmail.com>
2021-09-11 22:14:03 +0900

    add Programmers OpenChat

---
+++ src/Programmers/PGOpenChat.java
@@ -0,0 +1,42 @@
+package Programmers;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class PGOpenChat {
+
+    public static String[] solution(String[] record) {
+
+        int len = 0;
+        Map<String, String> nicks = new HashMap<>();
+
+        for(int i=0; i<record.length; i++) {
+            String msg = record[i].split(" ")[0];
+            if(msg.equals("Enter") || msg.equals("Change")) {
+                nicks.put(record[i].split(" ")[1], record[i].split(" ")[2]);
+            }
+        }
+
+        ArrayList<String> arr = new ArrayList<>();
+
+        for(int i=0; i<record.length; i++) {
+            String msg = record[i].split(" ")[0];
+            String uid = record[i].split(" ")[1];
+
+            if(msg.equals("Enter")) {
+                arr.add(nicks.get(uid) + "님이 들어왔습니다.");
+            } else if(msg.equals("Leave")) {
+                arr.add(nicks.get(uid) + "님이 나갔습니다.");
+            }
+        }
+
+        String[] answer = new String[arr.size()];
+        for(int i=0; i<answer.length; i++) {
+            answer[i] = arr.get(i);
+        }
+
+        return answer;
+    }
+}
+
