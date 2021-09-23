aeee1a8d6fe112e5823ebbc72365a1e7d466437a
tein408 <tein408@gmail.com>
2021-09-23 22:23:14 +0900

    add BOJ 2920

--- src/BOJ/P2920.java
+++ src/BOJ/P2920.java
@@ -1,41 +1,21 @@
 package BOJ;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class P2920 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String[] args) {
-		// 다장조는 c d e f g a b C, 총 8개 음으로 이루어져있다. 이 문제에서 8개 음은 다음과 같이 숫자로 바꾸어
-		// 표현한다. c는 1로, d는 2로, ..., C를 8로 바꾼다.
-		// 1부터 8까지 차례대로 연주한다면 ascending, 8부터 1까지 차례대로 연주한다면 descending, 둘 다 아니라면
-		// mixed 이다.
-		// 연주한 순서가 주어졌을 때, 이것이 ascending인지, descending인지, 아니면 mixed인지 판별하는 프로그램을
-		// 작성하시오.
-		
-		Scanner s = new Scanner(System.in);
-		int[] a = new int[8];
-//		for(int i=0; i<a.length; i++){
-//			a[i]=i+1;
-//		}
-		
-		int[] c = new int[8];
-		for(int i=0; i<a.length; i++){
-			c[i] = s.nextInt();
-		}
-		
-		
-				
-		for(int i=0; i<a.length; i++){
-			a[i]=i+1;
-			
-			if(c[i]<=i+1){
-				
-			}
-				
-		}
-		
-		
-		
-	}
+        String temp = bf.readLine();
+        String ascending = "1 2 3 4 5 6 7 8";
+        String descending = "8 7 6 5 4 3 2 1";
+        String result = temp.equals(ascending)? "ascending":
+                temp.equals(descending)? "descending":"mixed";
 
+        System.out.println(result);
+
+
+    }
 }
