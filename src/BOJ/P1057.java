dd8cb7401ccebcd8630c146a69dabe273078592b
tein408 <tein408@gmail.com>
2021-09-27 13:03:01 +0900

    refactor BOJ 1057
    - 더 간결한 풀이 추가

--- src/BOJ/P1057.java
+++ src/BOJ/P1057.java
@@ -16,37 +16,34 @@ public class P1057 {
 
         int cnt = 1;
 
-        while(true) {
+//        while(true) {
+//
+//            if(kim > lim) {
+//                int temp = kim;
+//                kim = lim;
+//                lim = temp;
+//            }
+//
+//            int cha = lim - kim;
+//            if(cha == 1 && lim%2 == 0) break;
+//
+//            if(kim%2 == 0) kim /= 2;
+//            else kim = kim/2 + 1;
+//
+//            if(lim%2 == 0) lim /= 2;
+//            else lim = lim/2 + 1;
+//
+//            cnt++;
+//        }
 
-            if(kim > lim) {
-                int temp = kim;
-                kim = lim;
-                lim = temp;
-            }
 
-            int cha = lim - kim;
+        //더 간결한 풀이
+        cnt = 0;
 
-            if(cha == 1) {
-                if(lim%2 == 0)
-                    break;
-            }
-
-            if(kim%2 == 0) {
-                kim /= 2;
-            } else {
-                kim = kim/2 + 1;
-            }
-
-            if(lim%2 == 0) {
-                lim /= 2;
-            } else {
-                lim = lim/2 + 1;
-            }
-
+        while (kim != lim) {
+            kim = kim/2 + kim%2;
+            lim = lim/2 + lim%2;
             cnt++;
-
-
-
         }
 
         System.out.println(cnt);
