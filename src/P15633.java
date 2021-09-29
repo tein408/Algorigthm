66a2dd52fbb6b206c56a79a35cf9cf627abf8186
tein408 <tein408@gmail.com>
2021-09-29 11:36:31 +0900

    add BOJ 15633

---
+++ src/P15633.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class P15633 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bf.readLine());
+        int result = 0;
+
+        for(int i=1; i<=n; i++) {
+            if(n%i == 0) {
+                result += i;
+            }
+        }
+
+        System.out.println(result * 5 - 24);
+
+
+    }
+}
