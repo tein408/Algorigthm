7a6b06781adf6117d4d353edebee6df73e622419
tein408 <tein408@gmail.com>
2021-09-30 16:57:19 +0900

    add BOJ 4949

---
+++ src/BOJ/P4949.java
@@ -0,0 +1,53 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class P4949 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        while(true) {
+            String str = bf.readLine();
+            if(str.equals("."))
+                break;
+            System.out.println(stack(str));
+        }
+
+    }
+
+    static String stack(String str) {
+        int len = str.length();
+        Stack<Character> stack = new Stack<>();
+
+        for(int i=0; i<len; i++) {
+            char temp = str.charAt(i);
+
+            if(temp == '(' || temp == '[')
+                stack.push(temp);
+
+            else if(temp == ')') {
+                if(stack.empty() || stack.peek() != '(') {
+                    return "no";
+                }
+                else stack.pop();
+            }
+
+            else if(temp == ']') {
+                if(stack.empty() || stack.peek() != '[') {
+                    return "no";
+                }
+                else stack.pop();
+            }
+
+        }
+
+        if(stack.isEmpty())
+            return "yes";
+        else
+            return "no";
+    }
+
+
+}
