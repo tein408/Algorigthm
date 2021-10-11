bf4ecd7a2dfd37b4603a71e5ea156bf5865db73d
tein408 <tein408@gmail.com>
2021-10-11 18:07:30 +0900

    add BOJ 10699

---
+++ src/BOJ/P10699.java
@@ -0,0 +1,14 @@
+package BOJ;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class P10699 {
+    public static void main(String[] args) throws Exception {
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = new Date();
+        System.out.println(format.format(date));
+
+    }
+}
