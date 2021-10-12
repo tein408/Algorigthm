a08a61c17f109991da6042fbb4b1455e5fe21150
tein408 <tein408@gmail.com>
2021-10-12 22:39:51 +0900

    add BOJ 17838

---
+++ src/BOJ/P17838.java
@@ -0,0 +1,31 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class P17838 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(bf.readLine());
+
+        for(int j=0; j<t; j++) {
+            String str = bf.readLine();
+
+            if(str.length() == 7) {
+                if(str.charAt(0) == str.charAt(1) && str.charAt(0) == str.charAt(4)
+                && str.charAt(0) != str.charAt(2) && str.charAt(2) == str.charAt(3)
+                && str.charAt(2) == str.charAt(5) && str.charAt(2) == str.charAt(6)
+                ) {
+                    System.out.println(1);
+                }
+                else {
+                    System.out.println(0);
+                }
+            } else {
+                System.out.println(0);
+            }
+        }
+
+
+    }
+}
