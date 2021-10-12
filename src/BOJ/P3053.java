f60c1503c02001d8e13fbd6b5d34c0b8e428f5a0
tein408 <tein408@gmail.com>
2021-10-12 19:27:38 +0900

    add BOJ 3053

---
+++ src/BOJ/P3053.java
@@ -0,0 +1,17 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class P3053 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        double r = Double.parseDouble(bf.readLine());
+        double c1 = Math.PI * r * r;
+        double c2 = 2 * r * r;
+
+        System.out.printf("%.6f\n", c1);
+        System.out.printf("%.6f", c2);
+
+    }
+}
