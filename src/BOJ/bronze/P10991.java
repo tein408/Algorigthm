b08fcc71ef1a09b2df2875472f6bef9be23a5c86
tein408 <tein408@gmail.com>
2021-11-02 23:02:05 +0900

    refactor BOJ.bronze 10991
    - 코드는 길지만 더 빠른 실행

--- src/BOJ/bronze/P10991.java
+++ src/BOJ/bronze/P10991.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class P10991 {
-    static String star = "* ";
+    static String star = "*";
     static String blank = " ";
     static String enter = "\n";
 
@@ -13,12 +13,29 @@ public class P10991 {
         StringBuffer sb = new StringBuffer();
         int n = Integer.parseInt(bf.readLine());
 
-        for(int i=1; i<=n; i++) {
-            for(int j=i; j<n; j++) {
+        for(int i=0; i<n; i++) {
+            for(int j=n-i-1; j>0; j--) {
                 sb.append(blank);
             }
-            for(int j=0; j<i; j++) {
-                sb.append(star);
+            for(int j=i; j>=0; j--) {
+                if(j==i || j==n)
+                    sb.append(star);
+                else if(j%2 == 0 && i%2 == 0)
+                    sb.append(star);
+                else if(j%2 == 1 && i%2 == 1)
+                    sb.append(star);
+                else
+                    sb.append(blank);
+            }
+            for(int j=1; j<=i; j++) {
+                if(j==i)
+                    sb.append(star);
+                else if(j%2 == 1 && i%2 == 1)
+                    sb.append(star);
+                else if(j%2 == 0 && i%2 == 0)
+                    sb.append(star);
+                else
+                    sb.append(blank);
             }
             sb.append(enter);
         }
