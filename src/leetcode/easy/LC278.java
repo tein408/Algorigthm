640906af5186cc317a37608ef0d45f50b70062ca
tein408 <tein408@gmail.com>
2021-11-02 12:03:25 +0900

    add leetcode 278

---
+++ src/leetcode/easy/LC278.java
@@ -0,0 +1,28 @@
+package leetcode.easy;
+
+public class LC278 {
+
+    boolean isBadVersion(int version) {
+        return true;
+    }
+
+    public int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+        int mid = 0;
+
+        while(left < right) {
+            mid = left + (right - left) / 2;
+
+            if(isBadVersion(mid)) {
+                right = mid;
+            }
+            else {
+                left = mid + 1;
+            }
+
+        }
+        return left;
+    }
+
+}
