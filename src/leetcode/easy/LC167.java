97ebbc19c2e3d25a1f2e790c4325e35ca7d97b66
EunHa Kang <tein408@gmail.com>
2021-11-04 16:51:28 +0900

    add leetcode.easy 167
    - Two Sum II - Input Array Is Sorted

---
+++ src/leetcode/easy/LC167.java
@@ -0,0 +1,34 @@
+package leetcode.easy;
+
+public class LC167 {
+
+    public static int[] twoSum(int[] numbers, int target) {
+        int left = 0, right = numbers.length - 1;
+        int sum = 0;
+        int[] res = new int[2];
+
+        while(left < right) {
+            res[0] = left + 1;
+            res[1] = right + 1;
+            sum = numbers[res[0]-1] + numbers[res[1]-1];
+
+            if(sum == target)
+                break;
+            else if(sum > target) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2,7,11,15};
+        int[] re = twoSum(arr, 9);
+        for(int i : re) {
+            System.out.println(i);
+        }
+    }
+
+}
