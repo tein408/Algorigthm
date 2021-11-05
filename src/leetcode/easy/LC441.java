b83623e080fff007e10d2dc2aaf4c9c4d59823fe
tein408 <tein408@gmail.com>
2021-11-05 13:49:07 +0900

    add leetcode.easy 441
    - 속도 및 메모리 개선
    - 등차수열 공식 이용

--- src/leetcode/easy/LC441.java
+++ src/leetcode/easy/LC441.java
@@ -1,40 +1,26 @@
 package leetcode.easy;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class LC441 {
 
     public static int arrangeCoins(int n) {
-        List<Long> list = new ArrayList<>();
-
-        list.add(1L);
-
-        for(int i=1; i<n; i++) {
-            long next = list.get(i-1) + i+1;
-            if(list.get(i-1) < Integer.MAX_VALUE) {
-                list.add(next);
-            } else {
-                break;
-            }
-        }
-
-        return find(list, n);
+        return find(n);
     }
 
-    static int find(List<Long> list, int n) {
-        int left = 0;
-        int right = list.size()-1;
+    static int find(int n) {
+        long left = 0;
+        long right = n;
 
         while (left <= right) {
-            int mid = (left + right) / 2;
-            if (n < list.get(mid)) {
+            long mid = left + (right - left) / 2;
+            //등차수열 공식 사용
+            long val = mid * (mid + 1) / 2;
+            if (n < val) {
                 right = mid - 1;
             } else {
                 left = mid + 1;
             }
         }
-        return left;
+        return (int) right;
     }
 
 }
