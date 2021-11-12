09d35dbb37c675e12a890769381edccc0ed14656
tein408 <tein408@gmail.com>
2021-11-12 11:41:56 +0900

    add leetcode.medium 877

---
+++ src/leetcode/medium/L877.java
@@ -0,0 +1,17 @@
+package leetcode.medium;
+
+public class L877 {
+
+    //return true; 만 해도 되던 문제
+    public boolean stoneGame(int[] piles) {
+        int alice = 0;
+        int bob = 0;
+
+/*        for(int i=0; i<piles.length/2; i++) {
+
+        }*/
+
+        return true;
+    }
+
+}
