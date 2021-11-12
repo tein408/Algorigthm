a9b89e71d0ab45ee7e7f8c54975be451c8d13334
tein408 <tein408@gmail.com>
2021-11-12 19:43:55 +0900

    add BOJ.bronze 15680

---
+++ src/BOJ/bronze/P15680.java
@@ -0,0 +1,12 @@
+package BOJ.bronze;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class P15680 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine());
+        System.out.println(n==0? "YONSEI":"Leading the Way to the Future");
+    }
+}
