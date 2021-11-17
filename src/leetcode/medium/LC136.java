6700b15ee8bfff05c06d6acd86471a3d49698c87
tein408 <tein408@gmail.com>
2021-11-17 19:24:13 +0900

    refactor leetcode.medium 136
    - 더 빠른 실행속도로 개선

--- src/leetcode/medium/LC136.java
+++ src/leetcode/medium/LC136.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class LC136 {
 
-    public int kthSmallest(int[][] matrix, int k) {
+    /*public int kthSmallest(int[][] matrix, int k) {
         int len = matrix.length * matrix[0].length;
         int[] arr = new int[len];
 
@@ -17,6 +17,39 @@ public class LC136 {
 
         Arrays.sort(arr);
         return arr[k-1];
+    }*/
+
+    //개선
+    public static int kthSmallest(int[][] matrix, int k) {
+        int a = matrix.length;
+        int b = matrix[0].length;
+        int left = matrix[0][0];
+        int right = matrix[a-1][b-1];
+        int mid;
+
+        while(left <= right) {
+            mid = left + (right - left) / 2;
+            int cnt = 0;
+            int j = b-1;
+
+            for(int i=0; i<a; i++) {
+                while(j >= 0 && matrix[i][j] > mid) {
+                    j--;
+                }
+                cnt += (j+1);
+            }
+
+            if(cnt < k)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return left;
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {{1,5,9}, {10,11,13}, {12,13,15}};
+        System.out.println(kthSmallest(matrix, 8));
     }
 
 }
