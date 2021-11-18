059b80a5fcf7683b617290abcc8367ba1a7e7fe6
tein408 <tein408@gmail.com>
2021-11-18 09:44:01 +0900

    refactor leetcode.easy 448
    - 각 원소의 등장 빈도를 확인하여 더 빠른 실행 속도로 개선

--- src/leetcode/easy/LC448.java
+++ src/leetcode/easy/LC448.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class LC448 {
     //Hash 이용
-    public List<Integer> findDisappearedNumbers(int[] nums) {
+/*    public List<Integer> findDisappearedNumbers(int[] nums) {
         HashSet<Integer> set = new HashSet<>();
         for(int i=0; i<nums.length; i++)
             set.add(nums[i]);
@@ -18,6 +18,24 @@ public class LC448 {
             else
                 list.add(i+1);
         }
+        return list;
+    }*/
+
+    //빈도 체크를 이용한 풀이
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        int[] cnt = new int[nums.length];
+        for(int i=0; i< nums.length; i++) {
+            cnt[nums[i]-1]++;
+        }
+
+        List<Integer> list = new ArrayList<>();
+        for(int i=0; i<nums.length; i++) {
+            if(cnt[i] > 0)
+                continue;
+            else
+                list.add(i+1);
+        }
+
         return list;
     }
 
