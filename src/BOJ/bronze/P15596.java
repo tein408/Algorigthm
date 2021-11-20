5e457d73a16b0412e82ab9f3398e78e924d7c929
tein408 <tein408@gmail.com>
2021-11-20 20:43:58 +0900

    chore 주석 수정

--- src/BOJ/bronze/P15596.java
+++ src/BOJ/bronze/P15596.java
@@ -1,7 +1,7 @@
 package BOJ.bronze;
 
 public class P15596 {
-    //class name : ignore.Test
+    //class name : Test
     long sum(int[] a) {
         long ans = 0;
         for(int i=0;i<a.length;i++) ans+=a[i];
