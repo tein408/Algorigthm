9869eddb0723ecc7cda0a0c33d2da09097943b18
tein408 <tein408@gmail.com>
2021-11-26 13:25:48 +0900

    add BOJ.silver 15649

---
+++ src/BOJ/silver/P15649.java
@@ -0,0 +1,46 @@
+package BOJ.silver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P15649 {
+
+    static int n, m;
+    static int[] arr;
+    static boolean[] visit;
+    static StringBuffer sb = new StringBuffer();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[m];
+        visit = new boolean[n+1];
+
+        solv(0);
+        System.out.println(sb);
+    }
+
+    static void solv(int cnt) {
+        if(cnt == m) {
+            for(int i=0; i<m; i++)
+                sb.append(arr[i]).append(" ");
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=1; i<=n; i++) {
+            if(!visit[i]) {
+                visit[i] = true;
+                arr[cnt] = i;
+                solv(cnt + 1);
+                visit[i] = false;
+            }
+        }
+
+    }
+
+}
