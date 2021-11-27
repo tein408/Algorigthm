d13e339467c4e5fcf5994f58eecb4adb5cbc2a91
tein408 <tein408@gmail.com>
2021-11-27 21:49:51 +0900

    add BOJ.silver 15665

---
+++ src/BOJ/silver/P15665.java
@@ -0,0 +1,54 @@
+package BOJ.silver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class P15665 {
+    static int n, m;
+    static int[] arr;
+    static int[] ans;
+    static StringBuffer sb = new StringBuffer();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+        ans = new int[n];
+
+        st = new StringTokenizer(bf.readLine(), " ");
+        for(int i=0; i<n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        solv(0, 0);
+        System.out.println(sb);
+    }
+
+    static void solv(int idx, int a) {
+        if(idx == m) {
+            for(int i=0; i<m; i++) {
+                sb.append(ans[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int temp = 0;
+
+        for(int i=0; i<n; i++) {
+            if(temp != arr[i]) {
+                ans[idx] = arr[i];
+                temp = arr[i];
+                solv(idx + 1, a+1);
+            }
+        }
+    }
+
+}
