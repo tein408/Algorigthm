060123505ec579e36c4635cadce7b407e775ebfe
tein408 <tein408@gmail.com>
2021-11-27 21:20:32 +0900

    add BOJ.silver 15654

---
+++ src/P15654.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class P15654 {
+    static int n, m;
+    static int[] arr;
+    static int[] ans;
+    static boolean[] visit;
+    static StringBuffer sb = new StringBuffer();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(bf.readLine(), " ");
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+        ans = new int[n];
+        visit = new boolean[n];
+
+        st = new StringTokenizer(bf.readLine(), " ");
+        for(int i=0; i<n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        solv(0);
+        System.out.println(sb);
+    }
+
+    static void solv(int cnt) {
+        if(cnt == m) {
+            for(int i=0; i<m; i++) {
+                sb.append(ans[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for(int i=0; i<n; i++) {
+            if(!visit[i]) {
+                visit[i] = true;
+                ans[cnt] = arr[i];
+                solv(cnt + 1);
+                visit[i] = false;
+            }
+        }
+    }
+
+}
