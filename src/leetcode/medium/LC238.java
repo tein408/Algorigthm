07b3e284851fc7117a5ea9882eee8fe1b63f4807
tein408 <tein408@gmail.com>
2021-11-27 18:00:16 +0900

    refactor leetcode.medium 238
    - 반복문을 줄이기

--- src/leetcode/medium/LC238.java
+++ src/leetcode/medium/LC238.java
@@ -1,8 +1,10 @@
 package leetcode.medium;
 
+import java.util.Arrays;
+
 public class LC238 {
 
-    public int[] productExceptSelf(int[] nums) {
+/*    public int[] productExceptSelf(int[] nums) {
         int[] ans = new int[nums.length];
         ans[0] = 1;
         //오른쪽 값 곱하고
@@ -18,6 +20,26 @@ public class LC238 {
             ans[i] = ans[i] * nums[i+1];
         }
         return ans;
+    }*/
+
+    //for문 2개
+    public int[] productExceptSelf(int[] nums) {
+        int[] ans = new int[nums.length];
+
+        int a = 1;
+        ans[0] = 1;
+
+        for(int i=1; i<nums.length; i++) {
+            ans[i] = ans[i-1] * nums[i-1];
+        }
+
+        for(int i=nums.length-1; i>=0; i--) {
+            ans[i] *= a;
+            a *= nums[i];
+        }
+
+        return ans;
     }
+
 
 }
