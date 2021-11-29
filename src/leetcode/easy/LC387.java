c7bbee5cfdbc7f2aabbfb526bc6086a78b569e25
tein408 <tein408@gmail.com>
2021-11-29 21:25:30 +0900

    add leetcode.easy 387

---
+++ src/leetcode/easy/LC387.java
@@ -0,0 +1,22 @@
+package leetcode.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LC387 {
+
+    public int firstUniqChar(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i=0; i<s.length(); i++) {
+            char temp = s.charAt(i);
+            map.put(temp, map.getOrDefault(temp, 0) + 1);
+        }
+        for(int i=0; i<s.length(); i++) {
+            char temp = s.charAt(i);
+            if(map.get(temp) == 1)
+                return i;
+        }
+        return -1;
+    }
+
+}
