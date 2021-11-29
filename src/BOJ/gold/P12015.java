e958ab0b5f90d04b21168124e681102c01f698db
tein408 <tein408@gmail.com>
2021-11-29 15:09:48 +0900

    add BOJ.gold 12015

---
+++ src/BOJ/gold/P12015.java
@@ -0,0 +1,53 @@
+package BOJ.gold;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class P12015 {
+
+    static int n;
+    static int[] arr;
+    static List<Integer> list = new ArrayList<>();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+
+        list.add(0);
+
+        n = Integer.parseInt(bf.readLine());
+        arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+
+        for(int i=0; i<n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            if(arr[i] > list.get(list.size()-1)) {
+                list.add(arr[i]);
+            }
+            else {
+                search(arr[i]);
+            }
+        }
+
+        System.out.println(list.size()-1);
+
+    }
+
+    static void search(int target) {
+        int left = 0;
+        int right = list.size() - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if(list.get(mid) >= target)
+                right = mid;
+            else
+                left = mid + 1;
+        }
+        list.set(right, target);
+    }
+
+}
