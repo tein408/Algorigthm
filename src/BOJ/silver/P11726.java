45858471e3f179f21f4caf2b9a3e21b28bd6a3de
tein408 <tein408@gmail.com>
2021-11-30 14:35:56 +0900

    add BOJ.silver 11726

---
+++ src/BOJ/silver/P11726.java
@@ -0,0 +1,25 @@
+package BOJ.silver;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class P11726 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine());
+
+        if(n < 2) {
+            System.out.println(n);
+            return;
+        }
+
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i=3; i<=n; i++) {
+            dp[i] = (dp[i-1] + dp[i-2]) % 10007;
+        }
+        System.out.println(dp[n]);
+    }
+}
