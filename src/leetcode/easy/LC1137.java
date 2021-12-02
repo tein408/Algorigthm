80f5b2a69cef782d3971b7800a4c6efaacf136e1
tein408 <tein408@gmail.com>
2021-12-02 23:50:34 +0900

    add leetcode.easy 1137

---
+++ src/leetcode/easy/LC1137.java
@@ -0,0 +1,20 @@
+package leetcode.easy;
+
+public class LC1137 {
+
+    public int tribonacci(int n) {
+        if(n < 2)
+            return n;
+        if(n == 2)
+            return 1;
+        int[] dp = new int[38];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+        for(int i=3; i<=n; i++) {
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        }
+        return dp[n];
+    }
+
+}
