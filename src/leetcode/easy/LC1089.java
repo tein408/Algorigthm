8c92cb10c6830eca0e52522d42bb76ad96b054db
tein408 <tein408@gmail.com>
2021-12-05 21:51:58 +0900

    add leetcode.easy 1089

---
+++ src/leetcode/easy/LC1089.java
@@ -0,0 +1,22 @@
+package leetcode.easy;
+
+import java.util.ArrayList;
+
+public class LC1089 {
+
+    public void duplicateZeros(int[] arr) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=0; i<arr.length; i++) {
+            if(arr[i] == 0) {
+                list.add(0);
+                list.add(0);
+            }
+            else {
+                list.add(arr[i]);
+            }
+        }
+        for(int i=0; i<arr.length; i++)
+            arr[i] = list.get(i);
+    }
+
+}
