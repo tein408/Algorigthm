9ff22ee0360f616d6b881ec3f0e5011fec494d29
tein408 <tein408@gmail.com>
2021-12-06 17:04:13 +0900

    add leetcode.easy 1217

---
+++ src/leetcode/easy/LC1217.java
@@ -0,0 +1,19 @@
+package leetcode.easy;
+
+public class LC1217 {
+
+    public int minCostToMoveChips(int[] position) {
+        int odd = 0;
+        int even = 0;
+
+        for(int i=0; i<position.length; i++) {
+            if(position[i]%2==0)
+                even++;
+            else
+                odd++;
+        }
+
+        return Math.min(even, odd);
+    }
+
+}
