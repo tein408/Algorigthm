2122666866657585e42781a890f67751509bf0ea
tein408 <tein408@gmail.com>
2021-12-12 16:01:47 +0900

    add leetcode.medium 1833

---
+++ src/LC1833.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class LC1833 {
+
+    public int maxIceCream(int[] costs, int coins) {
+        Arrays.sort(costs);
+        int cnt = 0;
+        for(int i=0; i<costs.length; i++) {
+            if(coins >= costs[i]) {
+                coins -= costs[i];
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+
+}
