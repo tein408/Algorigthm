45ca4a5b25528aa1a744a4d9ac4dfcbfa44cb66c
tein408 <tein408@gmail.com>
2021-12-13 09:14:17 +0900

    add leetcode.easy 1446

---
+++ src/LC1446.java
@@ -0,0 +1,18 @@
+public class LC1446 {
+
+    public int maxPower(String s) {
+        int left = 0, right = 0, max = 0;
+        char[] c = s.toCharArray();
+        while(left < s.length() && right < s.length()) {
+            if(c[left] == c[right]) {
+                max = Math.max(max, right - left + 1);
+                right++;
+            }
+            else {
+                left++;
+            }
+        }
+        return max;
+    }
+
+}
