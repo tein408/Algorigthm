9f03a8d0dda99b3df64890d6869a1b68abd8f986
tein408 <tein408@gmail.com>
2021-12-17 22:26:46 +0900

    refactor leetcode.medium 371
    - without using the operators `+` ans `-`

--- src/leetcode/medium/LC371.java
+++ src/leetcode/medium/LC371.java
@@ -1,7 +1,14 @@
 package leetcode.medium;
 
 public class LC371 {
+
+    //without using the operators + ans -
     public int getSum(int a, int b) {
-        return a+b;
+        while(b != 0) {
+            int c = a & b;
+            a = a ^ b;
+            b = c << 1;
+        }
+        return a;
     }
 }
