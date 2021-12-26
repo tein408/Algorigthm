590e9a558726660b06580d8ca791f7e8be914d6a
tein408 <tein408@gmail.com>
2021-12-26 20:33:57 +0900

    fix 변수 초기값 수정

--- src/leetcode/easy/LC171.java
+++ src/leetcode/easy/LC171.java
@@ -5,7 +5,7 @@ public class LC171 {
     public int titleToNumber(String columnTitle) {
         char[] c = columnTitle.toCharArray();
 
-        int ans = 1;
+        int ans = 0;
 
         for(int i=0; i<c.length; i++) {
             ans = ans * 26 + (c[i] - 'A' + 1);
