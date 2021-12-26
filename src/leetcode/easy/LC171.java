06b736057ccafd750672b4a384f91da86bff8916
tein408 <tein408@gmail.com>
2021-12-26 20:33:09 +0900

    add leetcode.easy 171

---
+++ src/leetcode/easy/LC171.java
@@ -0,0 +1,17 @@
+package leetcode.easy;
+
+public class LC171 {
+
+    public int titleToNumber(String columnTitle) {
+        char[] c = columnTitle.toCharArray();
+
+        int ans = 1;
+
+        for(int i=0; i<c.length; i++) {
+            ans = ans * 26 + (c[i] - 'A' + 1);
+        }
+
+        return ans;
+    }
+
+}
