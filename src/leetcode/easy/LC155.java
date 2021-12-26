7e0083f9eaf16c7670a17d0575343f07819fb787
tein408 <tein408@gmail.com>
2021-12-26 19:58:48 +0900

    add leetcode.easy 155

---
+++ src/leetcode/easy/LC155.java
@@ -0,0 +1,46 @@
+package leetcode.easy;
+
+import java.util.Stack;
+
+//MinStack
+public class LC155 {
+
+    Stack<Integer> stack;
+    int min = Integer.MAX_VALUE;
+
+    //MinStack
+    public LC155() {
+        stack = new Stack<>();
+    }
+
+    public void push(int val) {
+        if(min >= val) {
+            stack.push(min);
+            min = val;
+        }
+        stack.push(val);
+    }
+
+    public void pop() {
+        if(stack.pop() == min)
+            min = stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return min;
+    }
+
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
