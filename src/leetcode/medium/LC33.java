9b2d8ab327369acceadafbc0c193c080e8ddd3f0
tein408 <tein408@gmail.com>
2022-01-02 09:16:31 +0900

    refactor leetcode.medium 33
    - 주석 추가
    - 이해하기 더 쉬운 코드로 개선

--- src/leetcode/medium/LC33.java
+++ src/leetcode/medium/LC33.java
@@ -3,26 +3,54 @@ package leetcode.medium;
 public class LC33 {
 
     public int search(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length - 1;
+        // [2,1]
+        // target O, X
+
+        // Time Complexity O(log n)
+        // n/2, n/2 * 1/2 ...
+        // Space Complexity O(1)
+
+        // edge
+        // nums.length == 1
+        // target == nums[0] || target != nums[0]
+        if(nums.length == 1) {
+            if(nums[0] == target)
+                return 0;
+            return -1;
+        }
+
+        int left = 0, right = nums.length - 1;
 
         while(left <= right) {
             int mid = left + (right - left) / 2;
 
-            if(nums[mid] == target)
+            if(nums[left] == target)
+                return left;
+            else if(nums[right] == target)
+                return right;
+            else if(nums[mid] == target)
                 return mid;
-            else if(nums[left] <= nums[mid]) {
-                if(nums[left] <= target && target < nums[mid])
+
+            else if(nums[left] < nums[mid]) {
+                // [4,5,6,7,0,1,2] target 5
+                // [4,5,6,7,0,1,2] target 1
+                if(nums[left] < target && target < nums[mid]) {
                     right = mid - 1;
-                else
+                } else {
                     left = mid + 1;
-            } else if(nums[mid] <= nums[left]) {
-                if(nums[mid] < target && target <= nums[right])
+                }
+            } else {
+                // [5,6,7,1,2,3,4] target 5
+                // [5,6,7,1,2,3,4] target 3
+                if(nums[mid] < target && target <= nums[right]) {
                     left = mid + 1;
-                else
+                } else {
                     right = mid - 1;
+                }
             }
+
         }
+
         return -1;
     }
 
