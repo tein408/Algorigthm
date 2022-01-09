c7889a3a53f9fcb67ac5997135e8362950f7f2c1
tein408 <tein408@gmail.com>
2022-01-09 20:30:21 +0900

    add BOJ.bronze 24078
    - 불필요한 코드 삭제

--- src/BOJ/bronze/P24078.java
+++ src/BOJ/bronze/P24078.java
@@ -2,12 +2,10 @@ package BOJ.bronze;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class P24078 {
     public static void main(String[] args) throws Exception {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(bf.readLine());
-        System.out.println(n%21);
+        System.out.println(Integer.parseInt(bf.readLine())%21);
     }
 }
